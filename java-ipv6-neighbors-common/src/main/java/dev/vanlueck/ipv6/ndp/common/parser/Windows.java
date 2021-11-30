1fa27e72dbbd553616c71e0ba4959f6e38c0e723
Nils van Lück <nils@vanlueck.dev>
2021-11-30 18:51:26 +0100

    test: add testWindows

--- java-ipv6-neighbors-common/src/main/java/dev/vanlueck/ipv6/ndp/common/parser/Windows.java
+++ java-ipv6-neighbors-common/src/main/java/dev/vanlueck/ipv6/ndp/common/parser/Windows.java
@@ -7,23 +7,30 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.StringReader;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class Windows {
 
+    private Windows() {
+    }
+
     private static final String ERROR_MESSAGE = "String could not be parsed";
 
-    public Neighbor parse(String text) throws IOException, ParserException {
+    public static Set<Neighbor> parse(String text) throws IOException, ParserException {
         if (text == null) throw new ParserException(ERROR_MESSAGE);
+        Set<Neighbor> set = new HashSet<>();
 
         String[] neighborBlocks = text.split("\\n\\n");
         for (String neighborBlock : neighborBlocks) {
             BufferedReader bufReader = new BufferedReader(new StringReader(neighborBlock));
-            String line = null;
+            String line;
             Map<String, String> properties = new HashMap<>();
             while ((line = bufReader.readLine()) != null) {
-                String[] splittedLine = line.split(":", 1);
-                if (splittedLine.length != 2) throw new ParserException(ERROR_MESSAGE);
+                String[] splittedLine = line.split(":", 2);
+                if (splittedLine.length != 2)
+                    throw new ParserException(ERROR_MESSAGE);
 
                 String firstPart = splittedLine[0].trim();
                 String secondPart = splittedLine[1].trim();
@@ -34,10 +41,10 @@ public class Windows {
             String addressFamily = properties.get("AddressFamily");
             if (addressFamily == null) throw new ParserException(ERROR_MESSAGE);
 
-            if ("IPv6".equals(addressFamily)) continue;
+            if (!"IPv6".equals(addressFamily)) continue;
 
-            return Neighbor.fromWindowsHashMap(properties);
+            set.add(Neighbor.fromWindowsHashMap(properties));
         }
-        return null;
+        return set;
     }
 }
---
+++ java-ipv6-neighbors-common/src/test/java/dev/vanlueck/ipv6/ndp/common/parser/TestWindows.java
@@ -0,0 +1,239 @@
+package dev.vanlueck.ipv6.ndp.common.parser;
+
+import dev.vanlueck.ipv6.ndp.common.Neighbor;
+import dev.vanlueck.ipv6.ndp.common.exception.ParserException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.Set;
+
+class TestWindows {
+    @Test
+    @DisplayName("Test Windows Parser")
+    void testParse() {
+        String testText = """
+                    State                      : Permanent
+                    Store                      : ActiveStore
+                    AddressFamily              : IPv6
+                    ifIndex                    : 1
+                    Caption                    :
+                    Description                :
+                    ElementName                :
+                    InstanceID                 :
+                    CommunicationStatus        :
+                    DetailedStatus             :
+                    HealthState                :
+                    InstallDate                :
+                    Name                       : pp:<DD<55;55;
+                    OperatingStatus            :
+                    OperationalStatus          :
+                    PrimaryStatus              :
+                    Status                     :
+                    StatusDescriptions         :
+                    AvailableRequestedStates   :
+                    EnabledDefault             : 2
+                    EnabledState               : 5
+                    OtherEnabledState          :
+                    RequestedState             : 12
+                    TimeOfLastStateChange      :
+                    TransitioningToState       : 12
+                    CreationClassName          :
+                    SystemCreationClassName    :
+                    SystemName                 :
+                    AccessContext              : 0
+                    AccessInfo                 :
+                    InfoFormat                 :
+                    OtherAccessContext         :
+                    OtherInfoFormatDescription :
+                    InterfaceAlias             : Loopback Pseudo-Interface 1
+                    InterfaceIndex             : 1
+                    IPAddress                  : ff02::2
+                    LinkLayerAddress           :
+                    PSComputerName             :
+                    CimClass                   : ROOT/StandardCimv2:MSFT_NetNeighbor
+                    CimInstanceProperties      : {Caption, Description, ElementName, InstanceID...}
+                    CimSystemProperties        : Microsoft.Management.Infrastructure.CimSystemProperties
+                    
+                    State                      : Permanent
+                    Store                      : ActiveStore
+                    AddressFamily              : IPv4
+                    ifIndex                    : 22
+                    Caption                    :
+                    Description                :
+                    ElementName                :
+                    InstanceID                 :
+                    CommunicationStatus        :
+                    DetailedStatus             :
+                    HealthState                :
+                    InstallDate                :
+                    Name                       : <=C8<??8<??8<?:55<<55;
+                    OperatingStatus            :
+                    OperationalStatus          :
+                    PrimaryStatus              :
+                    Status                     :
+                    StatusDescriptions         :
+                    AvailableRequestedStates   :
+                    EnabledDefault             : 2
+                    EnabledState               : 5
+                    OtherEnabledState          :
+                    RequestedState             : 12
+                    TimeOfLastStateChange      :
+                    TransitioningToState       : 12
+                    CreationClassName          :
+                    SystemCreationClassName    :
+                    SystemName                 :
+                    AccessContext              : 0
+                    AccessInfo                 :
+                    InfoFormat                 :
+                    OtherAccessContext         :
+                    OtherInfoFormatDescription :
+                    InterfaceAlias             : vEthernet (WSL)
+                    InterfaceIndex             : 22
+                    IPAddress                  : 239.255.255.250
+                    LinkLayerAddress           : 01-00-5E-7F-FF-FA
+                    PSComputerName             :
+                    CimClass                   : ROOT/StandardCimv2:MSFT_NetNeighbor
+                    CimInstanceProperties      : {Caption, Description, ElementName, InstanceID...}
+                    CimSystemProperties        : Microsoft.Management.Infrastructure.CimSystemProperties
+                    
+                    State                      : Permanent
+                    Store                      : ActiveStore
+                    AddressFamily              : IPv4
+                    ifIndex                    : 22
+                    Caption                    :
+                    Description                :
+                    ElementName                :
+                    InstanceID                 :
+                    CommunicationStatus        :
+                    DetailedStatus             :
+                    HealthState                :
+                    InstallDate                :
+                    Name                       : <<>8:8<8@:55<<55;
+                    OperatingStatus            :
+                    OperationalStatus          :
+                    PrimaryStatus              :
+                    Status                     :
+                    StatusDescriptions         :
+                    AvailableRequestedStates   :
+                    EnabledDefault             : 2
+                    EnabledState               : 5
+                    OtherEnabledState          :
+                    RequestedState             : 12
+                    TimeOfLastStateChange      :
+                    TransitioningToState       : 12
+                    CreationClassName          :
+                    SystemCreationClassName    :
+                    SystemName                 :
+                    AccessContext              : 0
+                    AccessInfo                 :
+                    InfoFormat                 :
+                    OtherAccessContext         :
+                    OtherInfoFormatDescription :
+                    InterfaceAlias             : vEthernet (WSL)
+                    InterfaceIndex             : 22
+                    IPAddress                  : 224.0.2.60
+                    LinkLayerAddress           : 01-00-5E-00-02-3C
+                    PSComputerName             :
+                    CimClass                   : ROOT/StandardCimv2:MSFT_NetNeighbor
+                    CimInstanceProperties      : {Caption, Description, ElementName, InstanceID...}
+                    CimSystemProperties        : Microsoft.Management.Infrastructure.CimSystemProperties
+                    
+                    State                      : Permanent
+                    Store                      : ActiveStore
+                    AddressFamily              : IPv4
+                    ifIndex                    : 22
+                    Caption                    :
+                    Description                :
+                    ElementName                :
+                    InstanceID                 :
+                    CommunicationStatus        :
+                    DetailedStatus             :
+                    HealthState                :
+                    InstallDate                :
+                    Name                       : <<>8:8:8<?;55<<55;
+                    OperatingStatus            :
+                    OperationalStatus          :
+                    PrimaryStatus              :
+                    Status                     :
+                    StatusDescriptions         :
+                    AvailableRequestedStates   :
+                    EnabledDefault             : 2
+                    EnabledState               : 5
+                    OtherEnabledState          :
+                    RequestedState             : 12
+                    TimeOfLastStateChange      :
+                    TransitioningToState       : 12
+                    CreationClassName          :
+                    SystemCreationClassName    :
+                    SystemName                 :
+                    AccessContext              : 0
+                    AccessInfo                 :
+                    InfoFormat                 :
+                    OtherAccessContext         :
+                    OtherInfoFormatDescription :
+                    InterfaceAlias             : vEthernet (WSL)
+                    InterfaceIndex             : 22
+                    IPAddress                  : 224.0.0.251
+                    LinkLayerAddress           : 01-00-5E-00-00-FB
+                    PSComputerName             :
+                    CimClass                   : ROOT/StandardCimv2:MSFT_NetNeighbor
+                    CimInstanceProperties      : {Caption, Description, ElementName, InstanceID...}
+                    CimSystemProperties        : Microsoft.Management.Infrastructure.CimSystemProperties
+                    
+                    State                      : Permanent
+                    Store                      : ActiveStore
+                    AddressFamily              : IPv6
+                    ifIndex                    : 1
+                    Caption                    :
+                    Description                :
+                    ElementName                :
+                    InstanceID                 :
+                    CommunicationStatus        :
+                    DetailedStatus             :
+                    HealthState                :
+                    InstallDate                :
+                    Name                       : pp:<DD<55;55;
+                    OperatingStatus            :
+                    OperationalStatus          :
+                    PrimaryStatus              :
+                    Status                     :
+                    StatusDescriptions         :
+                    AvailableRequestedStates   :
+                    EnabledDefault             : 2
+                    EnabledState               : 5
+                    OtherEnabledState          :
+                    RequestedState             : 12
+                    TimeOfLastStateChange      :
+                    TransitioningToState       : 12
+                    CreationClassName          :
+                    SystemCreationClassName    :
+                    SystemName                 :
+                    AccessContext              : 0
+                    AccessInfo                 :
+                    InfoFormat                 :
+                    OtherAccessContext         :
+                    OtherInfoFormatDescription :
+                    InterfaceAlias             : Loopback Pseudo-Interface 1
+                    InterfaceIndex             : 1
+                    IPAddress                  : ff02::2
+                    LinkLayerAddress           :
+                    PSComputerName             :
+                    CimClass                   : ROOT/StandardCimv2:MSFT_NetNeighbor
+                    CimInstanceProperties      : {Caption, Description, ElementName, InstanceID...}
+                    CimSystemProperties        : Microsoft.Management.Infrastructure.CimSystemProperties
+                """;
+
+        try {
+            Set<Neighbor> neighbors = Windows.parse(testText);
+            Assertions.assertNotNull(neighbors);
+            Assertions.assertEquals(2, neighbors.size());
+            Neighbor neighbor = neighbors.iterator().next();
+            Assertions.assertNotNull(neighbor);
+
+        } catch (IOException | ParserException e) {
+            Assertions.fail(e);
+        }
+    }
+}
