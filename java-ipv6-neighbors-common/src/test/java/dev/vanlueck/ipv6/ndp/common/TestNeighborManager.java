8db337535315d395dc3d58c470a6839f3291c221
Nils van Lück <nils@vanlueck.dev>
2021-11-30 19:58:46 +0100

    test: add neighbormanager test

---
+++ java-ipv6-neighbors-common/src/test/java/dev/vanlueck/ipv6/ndp/common/TestNeighborManager.java
@@ -0,0 +1,41 @@
+package dev.vanlueck.ipv6.ndp.common;
+
+import dev.vanlueck.ipv6.ndp.api.Neighbor;
+import dev.vanlueck.ipv6.ndp.api.NeighborManager;
+import dev.vanlueck.ipv6.ndp.api.exception.ParserException;
+import dev.vanlueck.ipv6.ndp.api.exception.UnsupportedOSException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.Set;
+
+class TestNeighborManager {
+
+    @Test
+    @DisplayName("Test Neighbor Manager implementation sync")
+    void testNeighborManagerSync() {
+        try {
+            NeighborManager neighborManager = new NeighborManagerImpl();
+            Set<Neighbor> neighbors = neighborManager.getNeighbors();
+            Assertions.assertNotNull(neighbors);
+        } catch (UnsupportedOSException | ParserException | IOException e) {
+            Assertions.fail(e);
+        }
+    }
+
+    @Test
+    @DisplayName("Test Neighbor Manager implementation async")
+    void testNeighborManagerAsync() {
+        try {
+            NeighborManager neighborManager = new NeighborManagerImpl();
+            neighborManager.getNeighborsAsync().whenComplete((neighbors, throwable) -> {
+                if (throwable != null) Assertions.fail(throwable);
+                Assertions.assertNotNull(neighbors);
+            });
+        } catch (UnsupportedOSException e) {
+            Assertions.fail(e);
+        }
+    }
+}
